501146ec262616a17c4ec7f2d8500321ff9c8bba
Harry3693 <153526266+Harry3693@users.noreply.github.com>
2026-08-08 21:33:39 -0300

    Probar URLs page=N y rechazar formato anterior

--- batch-runner/test/seguridad.test.js
+++ batch-runner/test/seguridad.test.js
@@ -10,8 +10,8 @@ test('acepta páginas de búsqueda válidas de ZonaJobs', () => {
     'https://www.zonajobs.com.ar/empleos-busqueda-gerente-comercial.html'
   );
   assert.equal(
-    validarURLZonaJobs('https://www.zonajobs.com.ar/empleos-busqueda-gerente-comercial-pagina-5.html'),
-    'https://www.zonajobs.com.ar/empleos-busqueda-gerente-comercial-pagina-5.html'
+    validarURLZonaJobs('https://www.zonajobs.com.ar/empleos-busqueda-gerente-comercial.html?page=5'),
+    'https://www.zonajobs.com.ar/empleos-busqueda-gerente-comercial.html?page=5'
   );
 });
 
@@ -22,6 +22,13 @@ test('rechaza HTTP, otros dominios, puertos y rutas ajenas', () => {
     'https://www.zonajobs.com.ar.ejemplo.com/empleos-busqueda-gerente.html',
     'https://www.zonajobs.com.ar:8443/empleos-busqueda-gerente.html',
     'https://www.zonajobs.com.ar/',
+    'https://www.zonajobs.com.ar/empleos-busqueda-gerente-pagina-2.html',
+    'https://www.zonajobs.com.ar/empleos-busqueda-gerente.html?page=1',
+    'https://www.zonajobs.com.ar/empleos-busqueda-gerente.html?page=0',
+    'https://www.zonajobs.com.ar/empleos-busqueda-gerente.html?page=dos',
+    'https://www.zonajobs.com.ar/empleos-busqueda-gerente.html?page=2&orden=fecha',
+    'https://www.zonajobs.com.ar/empleos-busqueda-gerente.html?page=2&page=3',
+    'https://www.zonajobs.com.ar/empleos-busqueda-gerente.html?page=2#resultados',
     'https://127.0.0.1/empleos-busqueda-gerente.html',
     'file:///etc/passwd'
   ];
